4c6457f97f18c26fa37983f18fa8155bedd3b6d9
kingja51 <kingja51@gmail.com>
2024-12-17 17:28:34 +0900

    spring boot 셋팅 java 21

---
+++ src/test/java/com/pcms/springairag/SpringairagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcms.springairag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringairagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
